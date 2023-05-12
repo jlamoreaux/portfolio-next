9784e584bab9d4dcef77ac3bd0d8acf456fb4826
Jordan Lamoreaux <jlamoreaux@atlassian.com>
2023-05-11 22:13:16 -0500

    display 3 project cards on xl screen

--- app/components/GridContainer.tsx
+++ app/components/GridContainer.tsx
@@ -6,7 +6,7 @@ type GridContainerProps = {
 
 const GridContainer: FC<GridContainerProps> = ({ children }) => {
   return (
-    <div className="grid gap-12 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
+    <div className="grid gap-12 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
       {children}
     </div>
   );
