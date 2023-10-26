7c818eb0bfa533ec96a59fafa0a4e3bd9dd85beb
Jordan Lamoreaux <jlamoreaux@atlassian.com>
2023-10-26 09:23:13 -0500

    register bash lang for refractor

--- app/components/Code.tsx
+++ app/components/Code.tsx
@@ -3,9 +3,11 @@ import React, { FC } from "react";
 import Refractor from "react-refractor";
 import js from "refractor/lang/javascript";
 import ts from "refractor/lang/typescript";
+import bash from "refractor/lang/bash";
 
 Refractor.registerLanguage(js);
 Refractor.registerLanguage(ts);
+Refractor.registerLanguage(bash);
 
 type CodeProps = PortableTextComponentProps<{
   code: string;
